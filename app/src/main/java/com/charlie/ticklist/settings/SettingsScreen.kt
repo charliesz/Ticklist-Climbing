3fd00dffa19c697ee78e61ec38bb68c83a245939
Lukas Holl <Holl@nts-plan.local>
2026-09-02 16:14:33 +0200

    Fix settings screen status bar spacing

--- app/src/main/java/com/charlie/ticklist/settings/SettingsScreen.kt
+++ app/src/main/java/com/charlie/ticklist/settings/SettingsScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
@@ -39,6 +40,7 @@ fun SettingsScreen(
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
+                    .statusBarsPadding()
                     .padding(
                         horizontal = 12.dp,
                         vertical = 8.dp
